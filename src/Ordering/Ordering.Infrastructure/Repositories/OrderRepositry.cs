441e6612c2ae4a9ade88a42d347b6fa440701f1d
anand-tripathi-git <62899226+anand-tripathi-git@users.noreply.github.com>
2020-08-22 23:51:15 +0530

    Initial Commit

---
+++ src/Ordering/Ordering.Infrastructure/Repositories/OrderRepositry.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Core.Entities;
+using Ordering.Core.Repositories;
+using Ordering.Infrastructure.Data;
+using Ordering.Infrastructure.Repository.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Repositories
+{
+    public class OrderRepositry: Repository<Order>, IOrderRepository
+    {
+        private readonly OrderContext _dbContext;
+
+        public OrderRepositry(OrderContext dbContext): base(dbContext)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        public async Task<IEnumerable<Order>> GetOrderByUserName(string userName)
+        {
+            var orderList = await _dbContext.Orders.Where(x=>x.UserName==userName).ToListAsync();
+            return orderList;
+        }
+    }
+}
